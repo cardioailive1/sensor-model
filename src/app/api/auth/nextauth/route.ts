ed83807f505d957b7c55fa1bd49de155bc30c345
cardioailive1 <tonywell@cardioailive.com>
2026-07-07 15:35:53 -0400

    Delete src/app/api/auth/nextauth directory

--- src/app/api/auth/nextauth/route.ts
+++
@@ -1,7 +0,0 @@
-// NOTE: Rename this folder from "nextauth" to "[...nextauth]" after extracting.
-// GitHub cannot upload files with bracket characters in paths.
-
-import { handlers } from "../../../../lib/auth";
-
-export const GET = handlers.GET;
-export const POST = handlers.POST;
